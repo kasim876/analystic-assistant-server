68cc76553f38b3d8d069efd5d1b97a41cf50b50a
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2022-12-13 13:03:49 +0300

    edit db config

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_USER, process.env.DB_NAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
 });
